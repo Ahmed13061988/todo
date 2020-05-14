f181052f04e182edd5ec2f348f9226684c906d44
Ahmed <wolktheline@yahoo.com>
2020-05-13 18:01:58 -0600

    Done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,26 +1,49 @@
 class ItemsController < ApplicationController
+  before_action :find_item, only: [:show, :edit, :update, :destroy]
+
   def index 
     @items = Item.all
   end
 
   def new
+    @item = Item.new 
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def create
-    @item = Item.new(name: params[:name], description: params[:description])
-    
+    @item = Item.new(item_params)
+    if @item.save 
+      redirect_to item_path(@item)
+    else 
+      render :new 
+    end 
   end
 
   def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else 
+      render :edit
+    end 
   end
 
   def edit
   end
 
   def destroy
+    @item.destroy
+    redirect_to items_path
   end
+
+  private 
+  def item_params
+    params.require(:item).permit(:name, :description)
+  end 
+
+  def find_item
+    @item = Item.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get '/items', to: 'items#index'
-  get 'items/new'
-  get  '/items/:id', to: 'items#show'
-  get 'items/create'
-  get 'items/update'
-  get 'items/edit'
-  get 'items/destroy'
+  # get '/items', to: 'items#index'
+  # get 'items/new'
+  # get  '/items/:id', to: 'items#show'
+  # get 'items/create'
+  # get 'items/update'
+  # get 'items/edit'
+  # get 'items/destroy'
+  resources :items, except: :destroy 
+  delete "/items/:id", to: "items#delete", as: "delete_item"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
